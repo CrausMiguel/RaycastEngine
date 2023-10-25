7263de4903b26b1a82edc4f09c01e53e58fde3b5
CrausMiguel <vitor.melocraus@gmail.com>
2023-10-25 18:19:41 -0300

    Map base

--- RaycastEngine/src/Application.cpp
+++ RaycastEngine/src/Application.cpp
@@ -4,24 +4,50 @@
 
 #include <iostream>
 
-float px, py; //Player X and Y
 
+int mapX = 8, mapY = 8, mapS = 64;
+int map[] =
+{
+	1,1,1,1,1,1,1,1,
+	1,0,0,0,0,0,0,1,
+	1,0,1,0,0,0,0,1,
+	1,0,1,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,0,1,0,1,
+	1,0,0,0,0,0,0,1,
+	1,1,1,1,1,1,1,1,
+};
+
+void drawMap2D() {
+	for (int x = 0; x < mapX; x++)
+	{
+		for (int y = 0; y < mapY; y++)
+		{
+			if (map[x, y] == 1) { glColor3f(1, 1, 1); }
+			else { glColor3f(0, 0, 0); }
+		}
+	}
+}
+
+float px, py; //Player X and Y
 void drawPlayer() //Draw a player on the screen
 {
 	glColor3f(1, 1, 0); 
 	glPointSize(8);
 	glBegin(GL_POINTS);
-	glVertex2i(px, py);
+	glVertex2f(px, py);
 	glEnd();
 }
 
 void buttons(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (key == GLFW_KEY_A && action == GLFW_PRESS) px -= 5;
-	if (key == GLFW_KEY_D && action == GLFW_PRESS) px += 5;
-	if (key == GLFW_KEY_S && action == GLFW_PRESS) py -= 5;
-	if (key == GLFW_KEY_W && action == GLFW_PRESS) py += 5;
+	if (key == GLFW_KEY_A && action == GLFW_REPEAT) px -= 0.02f;
+	if (key == GLFW_KEY_D && action == GLFW_REPEAT) px += 0.02f;
+	if (key == GLFW_KEY_S && action == GLFW_REPEAT) py -= 0.02f;
+	if (key == GLFW_KEY_W && action == GLFW_REPEAT) py += 0.02f;
 }
+
+
 
 int main(void)
 {
@@ -56,10 +82,9 @@ int main(void)
 
 		drawPlayer();
 
-		glfwSetKeyCallback(window, buttons);
-
 		glfwSwapBuffers(window);
 
+		glfwSetKeyCallback(window, buttons);
 	
 
 		glfwPollEvents();
